a6499907b95815fe816d89399b5483e2ad447ddf
sidlery <yanneck.sidler@stud.hslu.ch>
2019-02-25 18:59:03 +0100

    initial

---
+++ Readme.txt
@@ -0,0 +1,8 @@
+***********************************************************************************************************************
+* Translate-Replace mit csv File:
+***********************************************************************************************************************
+
+
+1. Die ASCII Datei als .asc im Ordner des Skripts ablegen.
+2. Im CSV die erste Spalte mit den alten Translatecodes, in der zweiten die neuen korespondierenden Codes eintragen.
+3. Skript ausführen, es wird ein File replaced.txt geschrieben, welche wieder ins cad importiert werden kann.
---
+++ SearchReplace.ps1
@@ -0,0 +1,26 @@
+﻿$path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+$original_file = $path+'\*.asc'
+$destination_file =  $path+'\replaced.txt'
+
+$csvImport = Import-Csv -Path $path'\*.csv' -Header "col1","col2" -Delimiter ";"
+$HashTable = @{}
+foreach($r in $csvImport)
+{
+	$HashTable[$r.col1] = $r.col2
+}
+
+
+Get-Content -Path $original_file | ForEach-Object {
+    $line = $_
+    $HashTable.GetEnumerator() | ForEach-Object {
+        $pattern = "(\\[abcdABCD]{1})"+"("+$_.Key+"{1})"       
+        #es ist ein translate code und nichts anderes.
+        if ($line -match $pattern)             
+            
+            {
+                $line = $line -replace $_.Key , $_.Value  
+            }
+    }
+   $line
+} | Set-Content -Path $destination_file
